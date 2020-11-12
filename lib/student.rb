c471344bc8f9f33b1125793cd3e1f940ebda8f1a
Nicholas Merolle <nmerolle11@hotmail.com>
2020-11-12 23:17:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,6 +12,7 @@ class Student
   def self.create_from_collection(students_array)
      students_array.each do |student_hash|
       self.new(student_hash)
+    end
   end
 
   def add_student_attributes(attributes_hash)
